183f0845d5f9f4a0a59eb514cda94d9752ec82e4
tousif644 <tousif2567@gmail.com>
2022-05-07 08:17:45 +0600

    updated

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // defining port and calling express
 const port = process.env.PORT || 5000;
 const app = express();
-
+require('dotenv').config();
 // middle ware
 app.use(cors());
 app.use(express.json());
