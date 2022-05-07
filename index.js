fa9b44ef7b3c03c0d7434f110509e4122edb51ce
tousif644 <tousif2567@gmail.com>
2022-05-07 07:52:09 +0600

    port and middle ware implemented

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
+// defining port and calling express
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middle ware
+app.use(cors());
+app.use(express.json());
