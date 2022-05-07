04e2316a84fe0045ea053f2c40fbbe4ae159e771
tousif644 <tousif2567@gmail.com>
2022-05-07 07:54:55 +0600

    connected to mongo db and products api created

--- index.js
+++ index.js
@@ -11,7 +11,36 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// mongodb
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.blqzd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// async and await
+async function run() {
+  try {
+    await client.connect();
+
+    const productCollection = client
+      .db("warehouseManagement")
+      .collection("products");
+
+    //getting all the datas
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+      console.log("CONNECTED TO DB");
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Runing Warehouse Server");
